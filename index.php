1b9e3620d48b2edfa6901a95c7932684dfaf86eb
muhammad azis sularso <a2i3ssky@gmail.com>
2026-05-16 21:41:56 +0700

    bismillah

--- index.php
+++ index.php
@@ -1,13 +1,4 @@
 <?php
-include 'auth/login.php';
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
-
-// Cek apakah file database ada di folder lokal atau cloud
-if (file_exists('config/database.php')) {
-    include 'config/database.php';
-} else {
-    include '../config/database.php';
-}
+header('Location: auth/login.php');
+exit();
 ?>
